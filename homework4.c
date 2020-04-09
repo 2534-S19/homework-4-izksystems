9c3ca3bf9edccbedf5ecc21bf8e126453ff24559
Isaac <Isaac@IZK-Surface>
2020-04-09 19:39:51 -0400

    Moba working but string not sent yet.

--- homework4.c
+++ homework4.c
@@ -7,37 +7,83 @@ int main(void)
     char *response = "\n\n\r2534 is the best course in the curriculum!\r\n\n";
 
     // TODO: Declare the variables that main uses to interact with your state machine.
-
+    bool finished = false;
 
     // Stops the Watchdog timer.
     initBoard();
     // TODO: Declare a UART config struct as defined in uart.h.
     //       To begin, configure the UART for 9600 baud, 8-bit payload (LSB first), no parity, 1 stop bit.
-
+    eUSCI_UART_ConfigV1 uartConfig =
+    {
+     EUSCI_A_UART_CLOCKSOURCE_SMCLK,                           // 3MHz
+     19,                                                       // 312 / 16 = 19.5
+     8,                                                        // remainder of 312/5
+     0x55,                                                     // From table
+     EUSCI_A_UART_NO_PARITY,                                   // No parity
+     EUSCI_A_UART_LSB_FIRST,
+     EUSCI_A_UART_ONE_STOP_BIT,
+     EUSCI_A_UART_MODE,
+     EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,
+     EUSCI_A_UART_8_BIT_LEN,
+    };
 
     // TODO: Make sure Tx AND Rx pins of EUSCI_A0 work for UART and not as regular GPIO pins.
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P1,GPIO_PIN3,GPIO_PRIMARY_MODULE_FUNCTION);
+            //   Tx                              (uint_fast8_t selectedPort, uint_fast16_t selectedPins,uint_fast8_t mode);
 
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,GPIO_PIN2,GPIO_PRIMARY_MODULE_FUNCTION);
+            //   Rx                               (uint_fast8_t selectedPort, uint_fast16_t selectedPins,uint_fast8_t mode);
 
     // TODO: Initialize EUSCI_A0
-
+    UART_initModule(EUSCI_A0_BASE,&uartConfig);
+    //             (uint32_t moduleInstance,const eUSCI_UART_ConfigV1 *config);
 
     // TODO: Enable EUSCI_A0
-
+    UART_enableModule(EUSCI_A0_BASE);
+    //               (uint32_t moduleInstance);
 
     while(1)
     {
         // TODO: Check the receive interrupt flag to see if a received character is available.
         //       Return 0xFF if no character is available.
+        if (UART_getInterruptStatus(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG == EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG))
+                //                 (uint32_t moduleInstance,uint8_t mask);
+        {
+            rChar =  UART_receiveData(EUSCI_A0_BASE);
+               //                    (uint32_t moduleInstance);
+        }
+        else
+        {
+            rChar = 0xFF;
+        }
 
 
         // TODO: If an actual character was received, echo the character to the terminal AND use it to update the FSM.
         //       Check the transmit interrupt flag prior to transmitting the character.
-
+        if (rChar != 0xFF){
+            // Recieved a valid charachter.
+            charFSM(rChar);
+            if (UART_getInterruptStatus(EUSCI_A0_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) == EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) {
+            //                         (uint32_t moduleInstance,uint8_t mask);
+            UART_transmitData(EUSCI_A0_BASE, rChar);
+            //               (uint32_t moduleInstance,uint_fast8_t transmitData);
+            }
+        }
 
 
         // TODO: If the FSM indicates a successful string entry, transmit the response string.
         //       Check the transmit interrupt flag prior to transmitting each character and moving on to the next one.
         //       Make sure to reset the success variable after transmission.
+        if (charFSM == false)
+        {
+           if (UART_getInterruptStatus(EUSCI_A0_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) == EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG);
+           {
+               UART_transmitData(EUSCI_A0_BASE,*response);
+               UART_receiveData(EUSCI_A0_BASE);
+               //charFSM == false;
+           }
+
+        }
 
 
     }
@@ -51,8 +97,48 @@ void initBoard()
 // TODO: FSM for detecting character sequence.
 bool charFSM(char rChar)
 {
+    typedef enum {Start, Two, Five, Three, Four} startState;{
+        static int startState = Start;
+
+        switch (startState){
+
+        case Start:
+            if (rChar == '2')
+                startState = Two;
+            else
+                startState = Start;
+            break;
+
+        case Two:
+            if (rChar == '5' && startState == Two)
+                startState = Five;
+            else
+                startState = Start;
+            break;
+
+        case Five:
+            if (rChar == '3' && (startState == Five && startState != Start))
+                startState = Four;
+            else
+                startState = Start;
+            break;
+
+        case Three:
+            if (rChar == '4' && (startState == Three && startState != (Start | Four))){
+                startState = Four;
+            }
+            else{
+                startState = Start;
+            }
+            break;
+
+        case Four:
+            startState = Start;
+            break;
+
+        }
+
+    }
     bool finished = false;
-
-
     return finished;
 }
